0f838662c19d8070f6392488beff4010946a0ed6
Касимов Данияр <KasimovD@lcoast.itstep>
2023-09-05 21:34:31 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,87 @@
+
+
+# list=["apple","banna","orange","grape","pineaplle"]
+# list[3]="dgrgdgrgddddddddddd"
+# print(list)
+
+
+
+# a=int(input())
+# if a<15:
+#     print("rgldkrg")
+# else:
+#     print("sefkse")
+
+
+
+
+
+# for i in range(1,10+1):
+#     for j in range(1,10+1):
+#         print(i*j, end="\t")
+#     print()
+
+
+
+
+
+
+# num = lambda x :x+15
+# print(num(10))
+
+
+
+# import functools
+
+
+# list=[]
+# sum = functools.reduce(lambda x,y:x+y,list)
+# print(sum)
+
+
+
+
+# y = (lambda x : x-3)(5)
+# print(y)
+
+
+
+1
+2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

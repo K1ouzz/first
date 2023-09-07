56d1af8e349ad0cfb39c5040e7047fd95326a9f5
Касимов Данияр <KasimovD@lcoast.itstep>
2023-09-07 21:47:21 +0600

    work

--- main.py
+++ main.py
@@ -49,31 +49,97 @@
 
 
 
-class MyIterator:
-    def __init__(self,max_value):
-        self.max_value = max_value
-        self.current = 0
+# class MyIterator:
+#     def __init__(self,max_value):
+#         self.max_value = max_value
+#         self.current = 0
 
-    def __str__(self):
-        return "My name Iterator"
+#     def __str__(self):
+#         return "My name Iterator"
 
-    def __iter__(self):
-        print('iterator\n')
-        return self
+#     def __iter__(self):
+#         print('iterator\n')
+#         return self
 
-    def __next__(self):
-        print('next\n')
-        if self.current < self.max_value:
-            self.current += 1
-            return self.current
-        else:
-            raise StopIteration 
+#     def __next__(self):
+#         print('next\n')
+#         if self.current < self.max_value:
+#             self.current += 1
+#             return self.current
+#         else:
+#             raise StopIteration 
 
-my_iter = MyIterator(5)
-for num in my_iter:
-    print(num)
+# my_iter = MyIterator(5)
+# for num in my_iter:
+#     print(num)
 
     
+
+
+
+list = [10,20,30,40,50]
+list.append(60)
+list.remove(20)
+print(list)
+
+second_list = ("sef","fsef","efse")
+print(second_list[2])
+
+
+
+third_list = [1,2,3,4,5]
+dict={}
+for i in third_list:
+    dict[i]=i
+print(dict)
+
+
+
+second_dict = {"name":"Daniyar","age":18,"tel":"54654"}
+for i in second_dict:
+    print(i)
+print(second_dict["name"])
+print(second_dict.keys())
+print(second_dict.values())
+second_dict["xxx"]=1
+print(second_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
